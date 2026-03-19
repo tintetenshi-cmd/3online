62f38a2544077712c0c459d24bdefa548ee04747
martin <martin.lemee@apollossc.com>
2026-03-19 10:52:22 +0100

    fix(shared) 9

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,4 @@
-// Types
+// Types CORE
 export * from './types/core.js';
 
 // Utilitaires
@@ -7,7 +7,26 @@ export * from './utils/helpers.js';
 export * from './utils/cardColors.js';
 export * from './utils/constants.js';
 
-// EXPORTS EXPLICITES manquants
+// TOUS les exports explicites nécessaires
 export { AI_DIFFICULTY_CONFIGS } from './types/core.js';
-export type { ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData } from './types/core.js';
-export type { AIStrategy, AIDifficulty } from './types/core.js';
+export type { 
+  ClientToServerEvents, 
+  ServerToClientEvents, 
+  InterServerEvents, 
+  SocketData,
+  AIStrategy, 
+  AIDifficulty,
+  UUID, 
+  Player, 
+  GameState, 
+  GameAction, 
+  ActionResult, 
+  ChatMessage, 
+  RoomState, 
+  ConnectionStatus,
+  JoinResult,
+  LeaveResult,
+  StartResult,
+  GameMode,
+  AvatarType
+} from './types/core.js';
