98d83f786e0d078624ade6c308792de957e6acb2
martin <martin.lemee@apollossc.com>
2026-03-19 11:01:09 +0100

    fix(shared) 10

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,32 +1,23 @@
-// Types CORE
+// Core types
 export * from './types/core.js';
 
-// Utilitaires
+// Utils
 export * from './utils/validation.js';
 export * from './utils/helpers.js';
 export * from './utils/cardColors.js';
 export * from './utils/constants.js';
 
-// TOUS les exports explicites nécessaires
-export { AI_DIFFICULTY_CONFIGS } from './types/core.js';
+// VALUES (enums utilisables comme valeurs)
+export { 
+  AI_DIFFICULTY_CONFIGS, 
+  AvatarType, AIDifficulty, ConnectionStatus, GameStatus, RoomStatus, 
+  TurnResultType, GameMode, ActionType, CardLocation 
+} from './types/core.js';
+
+// TYPES (interfaces/types)
 export type { 
-  ClientToServerEvents, 
-  ServerToClientEvents, 
-  InterServerEvents, 
-  SocketData,
-  AIStrategy, 
-  AIDifficulty,
-  UUID, 
-  Player, 
-  GameState, 
-  GameAction, 
-  ActionResult, 
-  ChatMessage, 
-  RoomState, 
-  ConnectionStatus,
-  JoinResult,
-  LeaveResult,
-  StartResult,
-  GameMode,
-  AvatarType
+  ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData,
+  UUID, Player, GameState, GameAction, ActionResult, ChatMessage, RoomState,
+  JoinResult, LeaveResult, StartResult, AIStrategy, DecisionResult, ActionScore,
+  Card, TurnResult, VictoryResult, RoomInfo, RoomSettings, TurnPhase
 } from './types/core.js';
